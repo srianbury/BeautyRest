d9e0daba6b746bc7b19c87d21fb3ef0f5f662274
Brian Sunbury <bsunbury29@gmail.com>
2020-02-22 17:50:04 -0700

    added soup

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import os
 
 
 app = Flask(__name__)
@@ -11,7 +14,21 @@ VERSION = 'v1'
 
 class Main(Resource):
     def get(self):
-        return { 'hello': 'world' }
+        json_data = request.get_json(force=True)
+        url = json_data['url']
+
+        options = webdriver.ChromeOptions()
+        options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+        options.add_argument('--headless')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox')
+
+        driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=options)
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.close()
+        
+        return { 'soup': str(soup) }
 
 
 api.add_resource(Main, '/{VERSION}/soup'.format(VERSION=VERSION))
