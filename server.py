85b8be5c4c5169a9c4b85d352ab01d71706c18c9
Brian Sunbury <bsunbury29@gmail.com>
2020-02-22 18:26:17 -0700

    cors

--- server.py
+++ server.py
@@ -2,10 +2,12 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 import os
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
