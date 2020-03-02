b7bd9a792633de9742037e145cd9fc0fd067c6d6
Brian Sunbury <bsunbury29@gmail.com>
2020-03-01 20:34:01 -0700

    more tests

--- resources/Main/test.py
+++ resources/Main/test.py
@@ -6,6 +6,24 @@ class TestGetUrl(unittest.TestCase):
     def setUp(self):
         self.resource = Main()
 
+    
+    def test_plain(self):
+        expected = 'https://www.google.com/'
+        args = {
+            'url': 'https://www.google.com/'
+        }
+        actual = self.resource._get_url(args)
+        self.assertEqual(expected, actual)
+
+    
+    def test_with_query(self):
+        expected = 'https://www.google.com/search?q=mysearch'
+        args = {
+            'url': 'https://www.google.com/search?q=mysearch'
+        }
+        actual = self.resource._get_url(args)
+        self.assertEqual(expected, actual)
+
 
     def test_with_query_and_2_args(self):
         expected = 'https://www.google.com/search?q=mysearch&lang=eng&time=4'
@@ -16,7 +34,4 @@ class TestGetUrl(unittest.TestCase):
         }
         actual = self.resource._get_url(args)
         self.assertEqual(expected, actual)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    
