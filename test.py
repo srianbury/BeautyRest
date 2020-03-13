beafac0015b487834a93b15a52d4b0ec3fedea14
Brian Sunbury <bsunbury29@gmail.com>
2020-03-12 18:53:54 -0700

    required param test

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import unittest
+import json
 from server import app
+from common import constants
+from resources.Main.resource import Main
 
 
 class TestEndpoint(unittest.TestCase):
@@ -8,3 +11,14 @@ class TestEndpoint(unittest.TestCase):
     
     def test_should_run(self):
         self.assertEqual(True, True)
+
+    def test_missing_required_param_code(self):
+        expected = 400
+        actual = self.app.get(Main().route).status_code
+        self.assertEqual(expected, actual)
+
+    def test_missing_required_param_message(self):
+        expected = constants.ERROR_PARAM_URL
+        response = self.app.get(Main().route)
+        actual = response.json['message']['url']
+        self.assertEqual(expected, actual)
